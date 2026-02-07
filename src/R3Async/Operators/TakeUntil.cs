af75a3ae9a2cbf607d6de2ff7b99c5adfe010642
FedericoAlterio <federico.alterio@mail.polimi.it>
2026-02-07 20:56:01 +0100

    Optimized TakeUntil(CancellationToken)

--- src/R3Async/Operators/TakeUntil.cs
+++ src/R3Async/Operators/TakeUntil.cs
@@ -39,14 +39,7 @@ public static partial class AsyncObservable
 
         public AsyncObservable<T> TakeUntil(CancellationToken cancellationToken)
         {
-            TakeUntilStopSignalDelegate stopSignalSignal = notifyStop =>
-            {
-                var registration = cancellationToken.Register(x => ((Action<Result>)x).Invoke(Result.Success), 
-                                                              notifyStop);
-                return registration.ToAsyncDisposable();
-            };
-
-            return new TakeUntilFromRawSignal<T>(source, stopSignalSignal, TakeUntilOptions.Default);
+            return new TakeUntilCancellationToken<T>(source, cancellationToken);
         }
 
         public AsyncObservable<T> TakeUntil(TakeUntilStopSignalDelegate stopSignalSignal, TakeUntilOptions? options = null)
@@ -55,6 +48,117 @@ public static partial class AsyncObservable
                 throw new ArgumentNullException(nameof(stopSignalSignal));
 
             return new TakeUntilFromRawSignal<T>(source, stopSignalSignal, options ?? TakeUntilOptions.Default);
+        }
+    }
+
+    sealed class TakeUntilCancellationToken<T>(AsyncObservable<T> source, CancellationToken cancellationToken) : AsyncObservable<T>
+    {
+        readonly AsyncObservable<T> _source = source;
+        readonly CancellationToken _cancellationToken = cancellationToken;
+
+        protected override async ValueTask<IAsyncDisposable> SubscribeAsyncCore(AsyncObserver<T> observer, CancellationToken cancellationToken)
+        {
+            var subscription = new Subscription(this, observer);
+            try
+            {
+                await subscription.SubscribeAsync(cancellationToken);
+                return subscription;
+            }
+            catch
+            {
+                await subscription.DisposeAsync();
+                throw;
+            }
+        }
+
+        sealed class Subscription : IAsyncDisposable
+        {
+            readonly CancellationTokenSource _cts = new();
+            readonly TakeUntilCancellationToken<T> _parent;
+            readonly AsyncObserver<T> _observer;
+            readonly AsyncGate _gate = new();
+            readonly SingleAssignmentAsyncDisposable _subscription = new();
+            readonly SingleAssignmentAsyncDisposable _tokenRegistration = new();
+            readonly CancellationToken _disposeCancellationToken;
+
+            public Subscription(TakeUntilCancellationToken<T> parent, AsyncObserver<T> observer)
+            {
+                _parent = parent;
+                _observer = observer;
+                _disposeCancellationToken = _cts.Token;
+            }
+
+            public async ValueTask SubscribeAsync(CancellationToken cancellationToken)
+            {
+                var registration = _parent._cancellationToken.Register(OnTokenCanceled);
+                await _tokenRegistration.SetDisposableAsync(registration.ToAsyncDisposable());
+
+                var sourceSubscription = await _parent._source.SubscribeAsync(new SourceObserver(this), cancellationToken);
+                await _subscription.SetDisposableAsync(sourceSubscription);
+            }
+
+            async void OnTokenCanceled()
+            {
+                try
+                {
+                    await ForwardOnCompletedAsync(Result.Success);
+                }
+                catch
+                {
+                    // Ignored
+                }
+            }
+
+            async ValueTask ForwardOnNextAsync(T value, CancellationToken cancellationToken)
+            {
+                using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(_disposeCancellationToken, cancellationToken);
+                using (await _gate.LockAsync())
+                {
+                    await _observer.OnNextAsync(value, linkedCts.Token);
+                }
+            }
+
+            async ValueTask ForwardOnErrorResumeAsync(Exception error, CancellationToken cancellationToken)
+            {
+                using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(_disposeCancellationToken, cancellationToken);
+                using (await _gate.LockAsync())
+                {
+                    await _observer.OnErrorResumeAsync(error, linkedCts.Token);
+                }
+            }
+
+            async ValueTask ForwardOnCompletedAsync(Result result)
+            {
+                using (await _gate.LockAsync())
+                {
+                    await _observer.OnCompletedAsync(result);
+                }
+            }
+
+            public async ValueTask DisposeAsync()
+            {
+                await Task.Run(_cts.Cancel);
+                _cts.Dispose();
+                await _subscription.DisposeAsync();
+            }
+
+            sealed class SourceObserver(Subscription parent) : AsyncObserver<T>
+            {
+                protected override ValueTask OnNextAsyncCore(T value, CancellationToken cancellationToken)
+                {
+                    return parent.ForwardOnNextAsync(value, cancellationToken);
+                }
+
+                protected override ValueTask OnErrorResumeAsyncCore(Exception error, CancellationToken cancellationToken)
+                {
+                    return parent.ForwardOnErrorResumeAsync(error, cancellationToken);
+                }
+
+                protected override ValueTask OnCompletedAsyncCore(Result result)
+                {
+                    return parent.ForwardOnCompletedAsync(result);
+                }
+            }
         }
     }
 
