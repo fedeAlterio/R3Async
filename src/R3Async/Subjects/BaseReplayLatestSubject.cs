4a6936019da38020b2f19227aa0cd2dfdae0d20c
FedericoAlterio <federico.alterio@mail.polimi.it>
2026-02-28 11:33:04 +0100

    Incremented version

--- src/R3Async/Subjects/BaseReplayLatestSubject.cs
+++ src/R3Async/Subjects/BaseReplayLatestSubject.cs
@@ -1,9 +1,7 @@
-﻿using R3Async;
-using R3Async.Internals;
+﻿using R3Async.Internals;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
